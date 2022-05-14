2a5fabd7cdfc28af6cdc64bbf3b24eb87166cbe9
ajr0125 <51835753+ajr0125@users.noreply.github.com>
2022-05-14 12:35:05 -0400

    Delete GenerateClutterEdge.m

--- Data Generation Functions/GenerateClutterEdge.m
+++
@@ -1,7 +0,0 @@
-function [clutterEdgeData] = GenerateClutterEdge(noisedB, N, numTrials, numInterferers, CNR)
-    if(numInterferers==0)
-        clutterEdgeData = GenerateComplexGaussianNoise(noisedB, N, numTrials);
-    else
-        clutterEdgeData = GenerateComplexGaussianNoise(noisedB, N, numTrials, numInterferers, CNR);
-    end
-end
